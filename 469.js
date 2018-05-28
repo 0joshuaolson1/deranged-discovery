f102534cc09000c9d30869df387967c814f60de7
Joshua Olson <0joshuaolson1@gmail.com>
2018-05-28 17:21:12 -0500

    Begin ngram analysis

--- 469.js
+++ 469.js
@@ -1,1 +1,36 @@
 n=5e8;r=(c,m)=>u=>fetch(u='https://'+u,m).then(r=>r.text()).then(t=>c(u,t));c=l=>r((_,t,e=0,E=1)=>eval('_='+t).data.map(N=>{e=e%n+E*N;E*=256;e<E-E%n?(r((u,t,b=atob(t.slice(27,-26)),o=b.search(''))=>~o?new Set(b.substr(o+6,b.charCodeAt(o+4)-1).split('').map(d=>d.slice(d[1]=='*'?3:1))).forEach(r(open,{method:'HEAD'})):open(u.slice(0,16)+'opt=nometadata&i'+u.slice(16)))('crt.sh/?d='+e%n),e=e/n|0,E=E/n|0):E%=n}))('qrng.anu.edu.au/API/jsonI.php?type=uint8&length='+l)
+/*
+2
+x2: ')@((@(d@(t@)(@),@):@+u@,E@,b@,e@,m@.a@.m@/n@16@=e@=n@>r@E%@E*@E=@ap@ar@d=@da@e%@he@m)@ma@me@ng@n|@o+@od@p(@r=@t)@t,@t.@t=@u=@|0@})
+x3: %n@(o@(r@)=@,t@.t@0,@1)@6)@at@b.@h(@n(@op@pe@ta@u,
+x4: (u@ce@ch@e(@e=@et@ic@r(@sl@t(@th@u.
+x5: '+@('@='@en@li
+x6: ).
+x8: .s
+x9: =>
+x10: ))
+
+3
+x2: '+u@(o+@(r(@(u,@)('@))(@)))@)).@).t@,E=@,e=@,m)@,t,@.ma@.th@/n|@0,E@16)@6))@=>r@ap(@ata@b.s@dat@e%n@e=e@hen@map@met@n|0@ope@pen@r((@r(o@t.s@the@u,t@u.s@}))
+x3: )=>@='+@ch(@en(
+x4: .sl@ce(@ice@lic@sli
+
+4
+x2: (u,t@))('@).th@.map@.the@/n|0@0,E=@data@hen(@map(@open@then@u.sl
+x4: .sli@ice(@lice@slic
+
+5
+x2: ).the@.map(@.then@then(@u.sli
+x4: .slic@lice(@slice
+
+6
+x2: ).then@.then(@u.slic
+x4: .slice@slice(
+
+7
+x2: ).then(@u.slice
+x4: .slice(
+
+8
+x2: u.slice(
+*/
