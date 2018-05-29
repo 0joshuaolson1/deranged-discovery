44ace7ea84cbdd9ddb4601b05714da864edd47c9
Joshua Olson <0joshuaolson1@gmail.com>
2018-05-28 21:07:12 -0500

    Make siderules less distracting

--- c.js
+++ c.js
@@ -5,24 +5,24 @@ const request = (callback, method) => url =>
     .then(text => callback(url, text))
 c = rngByteCount =>
     request((_, text, random = 0, entropy = 1) =>
-/*  */  eval('_=' + text).data.map(rngByte => {
-/*  *   */  random = random % RANGE + entropy * rngByte
-/*  *   */  entropy *= 256
-/*  *   */  if(random < entropy - entropy % RANGE){
-/*  *   *   */  request((url, base64) => {
-/*  *   *   *   */  const blob = atob(base64.slice(27, -26))
-/*  *   *   *   */  const offset = blob.search('') // 0x11, 0x04
-/*  *   *   *   */  if(~offset)
-/*  *   *   *   *   */  new Set(
-/*  *   *   *   *   *   */  blob.substr(offset + 6, blob.charCodeAt(offset + 4) - 1)
-/*  *   *   *   *   *   */  .split('').map(domain => // 0x82
-/*  *   *   *   *   *   *   */  domain.slice(domain[1] == '*' ? 3 : 1)
-/*  *   *   *   *   *   */  )
-/*  *   *   *   *   */  ).forEach(request(open, {method: 'HEAD'}))
-/*  *   *   *   */  else open(url.slice(0, 16) + 'opt=nometadata&i' + u.slice(16))
-/*  *   *   */  )('crt.sh/?d=' + random % RANGE)
-/*  *   *   */  random = random/RANGE | 0
-/*  *   *   */  entropy = entropy/RANGE | 0
-/*  *   */  } else entropy %= RANGE
-/*  */  })
+        eval('_=' + text).data.map(rngByte => {
+            random = random % RANGE + entropy * rngByte
+      /* */ entropy *= 256
+            if(random < entropy - entropy % RANGE){
+                request((url, base64) => {
+      /* */   /* */ const blob = atob(base64.slice(27, -26))
+                    const offset = blob.search('') // 0x11, 0x04
+                    if(~offset)
+      /* */   /* */     new Set(
+                            blob.substr(offset + 6, blob.charCodeAt(offset + 4) - 1)
+                            .split('').map(domain => // 0x82
+      /* */   /* */             domain.slice(domain[1] == '*' ? 3 : 1)
+                            )
+                        ).forEach(request(open, {method: 'HEAD'}))
+      /* */   /* */ else open(url.slice(0, 16) + 'opt=nometadata&i' + u.slice(16))
+                })('crt.sh/?d=' + random % RANGE)
+                random = random/RANGE | 0
+      /* */     entropy = entropy/RANGE | 0
+            } else entropy %= RANGE
+        })
     )('qrng.anu.edu.au/API/jsonI.php?type=uint8&length='+l)
