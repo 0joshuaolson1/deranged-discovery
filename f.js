69484674f3ad9adf5eff7ba25f7d6680492cea9d
Joshua Olson <0joshuaolson1@gmail.com>
2018-06-20 17:59:14 -0500

    Chromium BigInts YAY

--- f.js
+++ f.js
@@ -1,1 +1,1 @@
-n=5e5;h='https://';(await(await fetch(h+'qrng.anu.edu.au/API/jsonI.php?&type=uint8&length=256')).json(e=0,E=1)).data.map(N=>{e=e%n+E*N;E*=256;e<E-E%n?(u=>fetch(u,{method:'HEAD'}).then(r=>r.ok?open(u):0))(h+'fimfiction.net/story/download/'+e%n+'/html',e=e/n|0,E=E/n|0):E%=n})
+N=500000n;h='https://';for(e=BigInt('0x'+await(await fetch(h+'qrng.anu.edu.au/API/jsonI.php?&type=hex16&length=1&size=256')).json(E=16n**512n).data[0]);E>=N;E/=N)(u=>fetch(u,{method:'HEAD'}).then(r=>r.ok&&open(u)))(h+'fimfiction.net/story/download/'+e%N+'/html')
