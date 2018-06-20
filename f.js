5fbed1e6bb3fb3137da0e31be61ac917e619d93c
Joshua Olson <0joshuaolson1@gmail.com>
2018-06-20 18:03:48 -0500

    reduce e too

--- f.js
+++ f.js
@@ -1,1 +1,1 @@
-N=500000n;h='https://';for(e=BigInt('0x'+await(await fetch(h+'qrng.anu.edu.au/API/jsonI.php?&type=hex16&length=1&size=256')).json(E=16n**512n).data[0]);E>=N;E/=N)(u=>fetch(u,{method:'HEAD'}).then(r=>r.ok&&open(u)))(h+'fimfiction.net/story/download/'+e%N+'/html')
+N=500000n;h='https://';for(e=BigInt('0x'+await(await fetch(h+'qrng.anu.edu.au/API/jsonI.php?&type=hex16&length=1&size=256')).json(E=16n**512n).data[0]);E>=N;e/=N,E/=N)(u=>fetch(u,{method:'HEAD'}).then(r=>r.ok&&open(u)))(h+'fimfiction.net/story/download/'+e%N+'/html')
