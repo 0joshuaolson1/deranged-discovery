9087189dd744c21e17384c8563420fa666edf484
Joshua Olson <0joshuaolson1@gmail.com>
2018-06-20 18:17:42 -0500

    Chromium BigInts YAY

--- s.js
+++ s.js
@@ -1,1 +1,1 @@
-n=5e8;S='.soundcloud.com/';h='https://';(await(await fetch(h+'qrng.anu.edu.au/API/jsonI.php?&type=uint8&length=512')).json(e=0,E=1)).data.map(N=>{e=e%n+E*N;E*=256;e<E-E%n?(u=>fetch(u,{method:'HEAD'}).then(r=>r.ok?open(u):0))(h+'w'+S+'player/?url='+h+'api'+S+'tracks/'+e%n,e=e/n|0,E=E/n|0):E%=n})
+n=5n<<30n;S='.soundcloud.com/';h='https://';for(e=BigInt('0x'+(await(await fetch(h+'qrng.anu.edu.au/API/jsonI.php?&type=hex16&length=1&size=256')).json(E=16n**512n).data[0]);E>=N;e/=N,E/=N)fetch(h+'w'+S+'player/?url='+h+'api'+S+'tracks/'+e%N,{method:'HEAD'}).then(r=>r.ok&&open(r.url))
