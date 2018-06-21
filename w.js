582e2c3323bd008954a136cd03f2bc20e30a34c8
Joshua Olson <0joshuaolson1@gmail.com>
2018-06-20 20:43:43 -0500

    work around Chrome DevTools limitation

--- w.js
+++ w.js
@@ -1,2 +1,3 @@
-try{N}catch(_){N=600000000n;e=0n;E=1n;h='https://';r=async(u,c)=>c(u=h+u,await(await fetch(u)).text())};r('qrng.anu.edu.au/API/jsonI.php?type=hex16&length=1&size=128',(_,t)=>{for(e+=E*BigInt('0x'+eval('_='+t).data[0]),E*=16n**512n;E>=N;e/=N,E/=N)r('crt.sh/?opt=nometadata&id='+e%N,(u,s,$='&nbsp;',_=$+$+$+$,b='<BR>'+_,e=b+_+_+_+'DNS:',i=s.search(b+_+_+'X509v3'+$+'Subject'+$+'Alternative'+$+'Name:'+$+e),j,S=new Set)=>{for(j=i+=~i?129:open(s.slice(-1)!=`
-`?u:I)();i==j;j=s.indexOf(e,i))S.add(s.slice(2*(s[i=j+104]=='*')+i,i=s.indexOf(b,i)));S.forEach(u=>console.log(h+u))})})
+try{N}catch(_){N=600000000n;e=0n;E=1n;h='https://';r=async(u,c)=>c(u=h+u,await(await fetch(u)).text())};r('qrng.anu.edu.au/API/jsonI.php?type=hex16&length=1&size=128',(_,t)=>{for(e+=E*BigInt('0x'+eval('_='+t).data[0]),E*=16n**256n;E>=N;e/=N,E/=N)r('crt.sh/?opt=nometadata&id='+e%N,(u,s,$='&nbsp;',_=$+$+$+$,b='<BR>'+_,e=b+_+_+_+'DNS:',i=s.search(b+_+_+'X509v3'+$+'Subject'+$+'Alternative'+$+'Name:'+$+e),j,S=new Set)=>{for(j=i+=~i?129:open(s.slice(-1)!=`
+`?u:I)();i==j;j=s.indexOf(e,i))S.add(s.slice(2*(s[i=j+104]=='*')+i,i=s.indexOf(b,i)));console.log(Array.from(S).join(`
+`))})})
