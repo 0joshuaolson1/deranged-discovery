2a1b07a2752755cfec56e1437cc107dce92caabb
Joshua Olson <0joshuaolson1@gmail.com>
2018-06-22 11:17:28 -0500

    Create b.js

---
+++ b.js
@@ -0,0 +1,1 @@
+try{N}catch(_){n=BigInt;N=n(4e9);e=n(0);E=n(1);h='https://';f=u=>{fetch(u=h+'bandcamp.com/EmbeddedPlayer/album='+e%N).then(r=>r.text()).then(t=>t[7665]!='v'&&open(u));e/=N;E/=N;E<N||setTimeout(f,81)}f(e+=E*n('0x'+(await(await fetch(h+'qrng.anu.edu.au/API/jsonI.php?&type=hex16&length=1&size=1024')).json(E*=n(16)**n(2048))).data[0]))
