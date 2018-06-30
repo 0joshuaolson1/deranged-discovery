7b6d028c72e8e1bbabf0d1fe6469b80d538422a4
Joshua Olson <0joshuaolson1@gmail.com>
2018-06-30 12:57:09 -0500

    Delete b.js

--- b.js
+++
@@ -1,1 +0,0 @@
-try{N}catch(_){n=BigInt;N=n(4e9);e=n(0);E=n(1);h='https://';f=(u,v)=>{v=e%N;for(_ of Array(32))fetch(u=h+'bandcamp.com/EmbeddedPlayer/album='+v++).then(r=>r.text()).then(t=>t[7665]!='v'&&open(u));e/=N;E/=N;E<N?alert():setTimeout(f,2e3)}}f(e+=E*n('0x'+(await(await fetch(h+'qrng.anu.edu.au/API/jsonI.php?&type=hex16&length=1&size=256')).json(E*=n(16)**n(512))).data))
